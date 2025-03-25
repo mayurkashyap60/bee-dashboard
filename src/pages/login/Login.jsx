280c154586c5d445cee1cb81ada0de9b71a761ec
mayurkashyap60 <mayurkashyap60@gmail.com>
2025-03-25 06:49:52 -0400

    Login and Registration process done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,51 +1,110 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
-    return ( 
+    const userEmail = useRef();
+    const userPassword = useRef();
+    const [error, setError] = useState(false);
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        let userEmailCheck = userEmail.current.value;
+        let userPassCheck = userPassword.current.value;
+
+        console.log(userEmailCheck);
+        console.log(userPassCheck);
+
+        try {
+            if (userEmailCheck == "" || userPassCheck == "") {
+                setError(true);
+            }
+            else {
+                setError(false);
+                const loginResponse = await axios.post('http://localhost:5000/api/auth/login', {
+                    username: userEmailCheck,
+                    password: userPassCheck
+                });
+
+                loginResponse.data && window.location.replace('/dashboard');
+            }
+        } catch (error) {
+            console.log(error);
+            setError(true);
+        }
+    };
+
+    return (
         <React.Fragment>
             <section className="_login">
-            <div className="_login_wrapper">
-                <div className="_left">
-                    <img src="./src/assets/images/loginbg.jpg" className="_loginbg" alt="loginimg" />
-                    <p className="_logo">Bee Logs</p>
-                    <a href="#" className="_website">Back to Website<span className="material-symbols-outlined">
-                            north_east
-                        </span></a>
-                    <h1 className="_text">Capturing Moments, <br /> Creating Memories</h1>
-                </div>
-                <div className="_right">
-                    <div className="_right_wrapper">
-                        <div className="_content">
-                            <h1 className="_heading">Login with an account</h1>
-                            <p className="_subheading">Don't have an account? <Link className="_loginCta" to="/register">Create</Link>
+                <div className="_login_wrapper">
+                    <div className="_left">
+                        <img
+                            src="./src/assets/images/loginbg.jpg"
+                            className="_loginbg"
+                            alt="loginimg"
+                        />
+                        <p className="_logo">Bee Logs</p>
+                        <a href="#" className="_website">
+                            Back to Website
+                            <span className="material-symbols-outlined">north_east</span>
+                        </a>
+                        <h1 className="_text">
+                            Capturing Moments, <br /> Creating Memories
+                        </h1>
+                    </div>
+                    <div className="_right">
+                        <div className="_right_wrapper">
+                            <div className="_content">
+                                <h1 className="_heading">Login with an account</h1>
+                                <p className="_subheading">
+                                    Don't have an account?{" "}
+                                    <Link className="_loginCta" to="/register">
+                                        Create
+                                    </Link>
+                                </p>
+                            </div>
+                            <div className="_form">
+                                <form autoComplete="off" onSubmit={handleLogin}>
+                                    <div className="_form_wrapper">
+                                        <div className="_formbox">
+                                            <input
+                                                type="text"
+                                                className="_field"
+                                                name="email"
+                                                id="email"
+                                                placeholder="Enter your username"
+                                                ref={userEmail}
+                                            />
+                                            {/* <span className="_error">This field is mandatory</span> */}
+                                        </div>
+                                        <div className="_formbox">
+                                            <input
+                                                type="text"
+                                                className="_field"
+                                                name="password"
+                                                id="password"
+                                                placeholder="Enter your password"
+                                                ref={userPassword}
+                                            />
+                                            {/* <span className="_error">This field is mandatory</span> */}
+                                        </div>
+                                        <div className="_formbox">
+                                            <span className="_error">
+                                                {error && <span>All fields is mandatory</span>}
+                                            </span>
+                                            <button className="_btn">Login</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <p className="_copyright">
+                                Copyright &copy; 2024. All right reserved.
                             </p>
                         </div>
-                        <div className="_form">
-                            <form autocomplete="off">
-                                <div className="_form_wrapper">
-                                    <div className="_formbox">
-                                        <input type="text" className="_field" name="email" id="email"
-                                            placeholder="Email address" />
-                                        <span className="_error">This field is mandatory</span>
-                                    </div>
-                                    <div className="_formbox">
-                                        <input type="text" className="_field" name="password" id="password"
-                                            placeholder="Enter your password" />
-                                        <span className="_error">This field is mandatory</span>
-                                    </div>
-                                    <div className="_formbox">
-                                        <button className="_btn">Login</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <p className="_copyright">Copyright &copy; 2024. All right reserved.</p>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
         </React.Fragment>
-     );
-}
- 
+    );
+};
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,40 @@
-import React, { Component } from 'react'
+import axios from 'axios';
+import React, { Component, useState } from 'react'
 import { Link } from 'react-router-dom';
 
 export const Register = () => {
+
+    const [userFname, setUserFname] = useState("");
+    const [userLname, setUserLname] = useState("");
+    const [useremail, setUseremail] = useState("");
+    const [userpass, setUserpass] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); 
+                
+        try { 
+            
+            if (userFname == "" || userLname == "" || useremail == "" ||  userpass == "") {
+                setError(true);
+            }
+            else{
+                let fullName = userFname + ' ' + userLname; 
+                const registerUser = await axios.post('http://localhost:5000/api/auth/register', {
+                    username: fullName,
+                    email: useremail,
+                    password:userpass
+                });
+                registerUser.data && window.location.replace('/login');
+            }
+
+        } catch (error) {
+            setError(true);
+        }
+
+    }
+
+
     return ( 
         <React.Fragment>
             <section className="_register">
@@ -22,29 +55,33 @@ export const Register = () => {
                             </p>
                         </div>
                         <div className="_form">
-                            <form autocomplete="off">
+                            <form onSubmit={handleSubmit} autoComplete="off">
                                 <div className="_form_wrapper">
                                     <div className="_formbox fname">
                                         <input type="text" className="_field" name="fname" id="fname"
-                                            placeholder="First name" />
-                                        <span className="_error">This field is mandatory</span>
+                                            placeholder="First name" onChange={e=>setUserFname(e.target.value)} />
+                                        {/* <span className="_error">{error && <span>This field is mandatory</span>}</span> */}
                                     </div>
                                     <div className="_formbox lname">
                                         <input type="text" className="_field" name="lname" id="lname"
-                                            placeholder="Last name" />
-                                        <span className="_error">This field is mandatory</span>
+                                            placeholder="Last name" onChange={e=>setUserLname(e.target.value)}/>
+                                        
+                                        {/* <span className="_error">{error && <span>This field is mandatory</span>}</span> */}
                                     </div>
                                     <div className="_formbox">
                                         <input type="text" className="_field" name="email" id="email"
-                                            placeholder="Email address" />
-                                        <span className="_error">This field is mandatory</span>
+                                            placeholder="Email address" onChange={e=>setUseremail(e.target.value)}/>
+                                        
+                                        {/* <span className="_error">{error && <span>This field is mandatory</span>}</span> */}
                                     </div>
                                     <div className="_formbox">
                                         <input type="text" className="_field" name="password" id="password"
-                                            placeholder="Enter your password" />
-                                        <span className="_error">This field is mandatory</span>
+                                            placeholder="Enter your password" onChange={e=>setUserpass(e.target.value)}/>
+                                        
+                                        {/* <span className="_error">{error && <span>This field is mandatory</span>}</span> */}
                                     </div>
                                     <div className="_formbox">
+                                    <span className="_error">{error && <span>All fields is mandatory</span>}</span>
                                         <button className="_btn">Create account</button>
                                     </div>
                                 </div>
